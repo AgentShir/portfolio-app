54671fc1800cff54336ba04dc13d2d5cd7bbb0ef
Shireen Garcia <garciasal@gmail.com>
2017-08-31 11:27:00 -0700

    Second portfolio app project, adding Contacts and References

---
+++ src/components/Contacts.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Contacts extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Link to="/about">
+          <button type="button" className="btn btn-outline-primary">
+            About
+          </button>
+        </Link>
+        <p>Great video, just watch it.</p>
+        <iframe name="youtube" title="fuchigami" width="560" height="315" src="https://www.youtube.com/embed/OS1jBjtHodk?rel=0" frameborder="0" allowfullscreen></iframe>
+      </div>
+    );
+  }
+}
+
+
+export default Contacts;
